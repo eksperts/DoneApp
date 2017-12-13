8c8817174e22e476838ee75537dad27c40e5af02
Uldis Zeidurs <uldis.zeidurs@gmail.com>
2017-12-13 12:07:19 +0100

    this crashes HARD too

--- Model/Timer.js
+++ Model/Timer.js
@@ -1,10 +1,23 @@
 import DI from 'FuseJS/DI'
+import moment from 'Model/moment'
+import T from 'FuseJS/Timer'
+
+let oldTimerDelete = T.delete;
+T.delete = function() {
+	console.log((new Error).stack);
+
+	return oldTimerDelete.apply(this, arguments)
+}
+
+let loops = new Map()
 
 export default class Timer {
 	constructor(tags = DI("tags"), storage = DI("storage")) {
 		this.tags = tags
 		this.storage = storage
 		this.running = false
+		this.started = null
+		this.elapsed = "00:00"
 		// TODO: this here should check if there was an open timer running
 	}
 
@@ -28,13 +41,22 @@ export default class Timer {
 	}
 
 	stopTimer() {
+		if (!loops.has(this)) return;
 		// TODO: log the current record as finished
 		this.running = false
+		this.started = null
+		clearTimeout(loops.get(this))
 	}
 
 	startTimer() {
 		// TODO: make a note a new current record started
 		this.running = true
+		this.started = moment()
+		loops.set(this, setTimeout(() => {
+			this.setElapsed(Math.floor((moment() - this.started) / 1000))
+		}, 1000))
 	}
+
+	setElapsed(v) { this.elapsed = v}
 
 }
